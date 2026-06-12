74078fb3bc7fe3a6387ed5e0756d37b36aa0f318
AbdullahHamoda20 <abodhamoda20@gmail.com>
2026-06-13 01:27:38 +0300

    update in AppValidations

--- lib/core/shared/AppValidations.dart
+++ lib/core/shared/AppValidations.dart
@@ -53,4 +53,73 @@ abstract class AppValidations {
     }
     return null;
   }
+
+  static String? validateAge(String? data) {
+    if (data == null || data.isEmpty) {
+      return "age can't be empty";
+    }
+
+    final age = int.tryParse(data);
+    if (age == null) {
+      return "age must be a number";
+    }
+
+    if (age < 10 || age > 100) {
+      return "The field Age must be between 10 and 100.";
+    }
+
+    return null;
+  }
+
+
+  static String? validateWeight(String? data) {
+    if (data == null || data.isEmpty) {
+      return "weight can't be empty";
+    }
+
+    final weight = double.tryParse(data);
+    if (weight == null) {
+      return "weight must be a number";
+    }
+
+    if (weight < 20 || weight > 300) {
+      return "The field Weight must be between 20 and 300.";
+    }
+
+    return null;
+  }
+
+  static String? validateHeight(String? data) {
+    if (data == null || data.isEmpty) {
+      return "height can't be empty";
+    }
+
+    final height = double.tryParse(data);
+    if (height == null) {
+      return "height must be a number";
+    }
+
+    if (height < 100 || height > 250) {
+      return "The field Height must be between 100 and 250.";
+    }
+
+    return null;
+  }
+
+  static String? validateBMI(String? data) {
+    if (data == null || data.isEmpty) {
+      return "BMI can't be empty";
+    }
+
+    final bmi = double.tryParse(data);
+    if (bmi == null) {
+      return "BMI must be a number";
+    }
+
+    if (bmi < 10 || bmi > 60) {
+      return "The field BMI must be between 10 and 60.";
+    }
+
+    return null;
+  }
 }
