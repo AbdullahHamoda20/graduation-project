18c404020a237e9162f2f621e3da65ad8ef5ece3
AbdullahHamoda20 <abodhamoda20@gmail.com>
2026-06-13 01:21:58 +0300

    Update app_colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -14,6 +14,18 @@ class AppColors {
     stops: [0.0, 0.4087, 1.0],
   );
 
+  static const LinearGradient  resultGradient = LinearGradient(
+    colors: [
+      Color(0xffFFF6F7),
+      Color(0xffFBECEF),
+      Color(0xffF7DDE2),
+      Color(0xffF3C7D5),
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    stops: [0.0, 0.2, .3,1.0],
+  );
+
   // Onboarding Screen
   static const  Color header = Color(0xffD63F67);
   static const  Color description = Color(0xffA8A8A8);
